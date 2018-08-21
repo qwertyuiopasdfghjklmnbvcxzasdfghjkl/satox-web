78a49e4bf49e8ea35b3c69e72823ff964287e565
15012773825 <pengmingxing.xu@qq.com>
2018-08-21 16:32:59 +0800

    去除bitark字眼。

--- src/public/index/banner02.vue
+++ src/public/index/banner02.vue
@@ -6,9 +6,9 @@
         <a href="javascript:;" target="_blank">
             <img :src="image3"/>
         </a>
-        <a href="javascript:;" target="_blank">
+        <router-link :to="{name:'otc_index'}">
             <img :src="image2"/>
-        </a>
+        </router-link>
     </div>
 </template>
 
@@ -16,11 +16,9 @@
 import {mapGetters} from 'vuex'
 import otcEN from '@/assets/images/home/banner-otc-en.png'
 import otcZH from '@/assets/images/home/banner-otc-zh.png'
-import forumZHCN from '@/assets/images/home/banner-forum-zh-cn.png'
-import forumZHTW from '@/assets/images/home/banner-forum-zh-tw.png'
-import forumEN from '@/assets/images/home/banner-forum-en.png'
 import newEN from '@/assets/images/home/banner-new-en.png'
 import newZH from '@/assets/images/home/banner-new-zh.png'
+import exchange from '@/assets/images/home/banner-exchange.png'
 export default {
   computed: {
     ...mapGetters(['getLang']),
@@ -32,13 +30,7 @@ export default {
       }
     },
     image2 () {
-      if (this.getLang === 'zh-CN') {
-        return forumZHCN
-      } else if (this.getLang === 'cht') {
-        return forumZHTW
-      } else {
-        return forumEN
-      }
+      return exchange
     },
     image3 () {
       if (this.getLang === 'zh-CN' || this.getLang === 'cht') {
