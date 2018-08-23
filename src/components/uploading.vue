b3a8233676ba0665a52acfb94ffb90a1e7b01207
15012773825 <pengmingxing.xu@qq.com>
2018-08-23 11:29:49 +0800

    完善二次认证上传加载中图标问题。

--- src/components/uploading.vue
+++ src/components/uploading.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="uploading">
     <div class="uploading-container">
-      <p>{{$t('public0.public188')}}</p>
+      <p>
+        <loading :size="50"></loading>
+        <span>{{$t('public0.public188')}}</span>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
+import loading from '@/components/loading'
+export default {
+  components: {
+    loading
+  }
+}
 </script>
 
 <style>
 .uploading{display: flex;justify-content: center;align-items: center;position: fixed;top: 0; right: 0;bottom: 0;left: 0;background-color: rgba(0, 0, 0, 0.5);}
 .uploading-container{padding: 40px;background-color: #404b69;}
-.uploading-container p{height: 30px;line-height: 30px;font-size: 16px;color: #becbe8;text-indent: 46px;background: url(../assets/images/loading.gif) no-repeat -8px center / 50px 50px;}
+.uploading-container p{height: 30px;line-height: 30px;font-size: 16px;color: #becbe8;display:flex;align-items:center;}
+.uploading-container div{margin-right:10px;}
 </style>
