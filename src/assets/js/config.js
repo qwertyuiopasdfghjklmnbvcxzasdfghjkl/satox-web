26d3027a19e8f3f493601b3857602e1800262bbd
15012773825 <pengmingxing.xu@qq.com>
2018-08-29 14:56:56 +0800

    优化。

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,7 +1,7 @@
 let domain = process.env.NODE_ENV === 'development' ? 'dev.koall.io' : location.host.split(':')[0]
 const config = {
   domain: domain,
-  origin: `${window.location.protocol}//${domain}/`,
+  origin: `${window.location.protocol}//${domain}`,
   imageType: /\.(jpg|png|jpeg|bmp)/i,
   protocol: window.location.protocol === 'https:' ? 'wss://': 'ws://'
 }
