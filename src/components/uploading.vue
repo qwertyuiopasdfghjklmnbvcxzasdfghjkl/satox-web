cd5c95f387de3fb0499152e41bb064d3c1c40aca
15012773825 <pengmingxing.xu@qq.com>
2018-09-04 11:56:12 +0800

    增加区号中国台湾(+886)、马来西亚(+60)。

--- src/components/uploading.vue
+++ src/components/uploading.vue
@@ -18,7 +18,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .uploading{display: flex;justify-content: center;align-items: center;position: fixed;top: 0; right: 0;bottom: 0;left: 0;background-color: rgba(0, 0, 0, 0.5);}
 .uploading-container{padding: 40px;background-color: #404b69;}
 .uploading-container p{height: 30px;line-height: 30px;font-size: 16px;color: #becbe8;display:flex;align-items:center;}
