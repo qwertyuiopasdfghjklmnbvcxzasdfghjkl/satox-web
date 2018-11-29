a8c88688c33362ee5caaaf78aadd938d8e18c525
李利军 <13587611202@koall.io>
2018-11-29 17:22:51 +0800

    去除otc广告限定价格
    Signed-off-by: 李利军 <13587611202@koall.io>

--- src/otc/otchome/createorder.vue
+++ src/otc/otchome/createorder.vue
@@ -397,12 +397,7 @@ export default {
           this.formData.symbol_count = utils.removeEndZero(numUtils.BN(res.symbol_count).toFixed(5))
           this.formData.min_amount = utils.removeEndZero(numUtils.BN(res.min_amount).toFixed(parseInt(res.ad_type) === 1 ? 5 : 2))
           this.formData.max_amount = utils.removeEndZero(numUtils.BN(res.max_amount).toFixed(parseInt(res.ad_type) === 1 ? 5 : 2))
-          // 当前广告币种为ATN时的处理
-          if (this.isATN) {
-            this.formData.price_type = 1
-          } else {
-            this.formData.price_type = res.price_type
-          }
+          this.formData.price_type = 1
           // 获取Symbol或Currency的最小交易限额
           this.fnGetSymbolAndCurrency(false, res.ad_type)
         }, (msg) => {
