4316426db57ae94a31afb2ec08eb864a1acf2dfb
yangl <574748221@qq.com>
2019-04-25 15:04:39 +0800

    登录 中英文版 提示

--- src/locale/lang/en-US.js
+++ src/locale/lang/en-US.js
@@ -1,10 +1,11 @@
 export const login = {
     login: 'LOGIN',
     welcome: 'welcome',
-    changeLang: '切换语言'
+    changeLang: '切换语言',
+    v: 'English version'
 }
 export const finance = {
     title: 'TBSH',
     welcome: '欢迎登录',
-    changeLang: 'LangSwitcher'
+    changeLang: 'LangSwitcher',
 }
