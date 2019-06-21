ce015da6c7c79ce116d50e00cb24b7767ab06b71
yangl <574748221@qq.com>
2019-06-21 15:47:21 +0800

    bug处理

--- src/shop/clause.vue
+++ src/shop/clause.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="cont">
     <p class="title">{{$t('shop.product_terms')}}</p>
+    <p class="p">
+      <router-link :to="{name: 'shop_pay', params: {total:totals}}">{{$t('shop.return_payment_page')}}</router-link>
+    </p>
     <div class="box clause">
       <h4>{{$t('shop.card_terms')}}</h4>
       <p v-html="$t('shop.cart_term_contents')"></p>
@@ -13,11 +16,21 @@
 </template>
 
 <script>
-    export default {
-        name: "clause"
+  export default {
+    data() {
+      return {
+        totals: null
+      }
+    },
+    created() {
+      this.totals = this.$route.params.total;
     }
+  }
 </script>
 
 <style scoped lang="less">
-@import "shop";
+  @import "shop";
+  .p{
+    padding: 10px;
+  }
 </style>
